17449d61fccf4c83be5fb3ba9b9a9dd1f6b12592
Kudaibergen-maker <kukubek99@gmail.com>
2024-11-28 23:28:28 +0600

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -1,54 +1,44 @@
+package com.example;
+
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Calculator {
+public class    Calculator {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        int res;
-
-        System.out.print("Введите перове целое число: ");
-        int num1 = sc.nextInt();
-
-        System.out.print("Выберите + - * /;");
-        String ac = sc.nextLine();
-        ac = sc.nextLine();
-        System.out.print("Введите перове целое число: ");
-        int num2 = sc.nextInt();
-        if (num1< 1 || num1 > 10 || num2 < 1 || num2 > 10)return; {
-            System.out.print ("");
+        System.out.print("Введите : ");
+        String input = sc.nextLine().trim();
+        try {
+            String result = calc(input);
+            System.out.println("Результат: " + result);
+        } catch (Exception e) {
+            System.out.println("Результат: " + e.getMessage());
         }
+    }
+public static String calc(String input) throws Exception {
 
+    {
+}
+        String regex = ".*\\b([1-9]|10)\\s+[-+*/]\\s+([1-9]|10)\\b.*";
 
+        Pattern pattern = Pattern.compile(regex);
 
-        switch (ac) {
-            case "+":
-                res = num1 + num2;
-                System.out.println("Результат:" + res);
-                break;
-            case "-":
-                res = num1 - num2;
-                System.out.println("Результат:" + res);
-                break;
-            case "*":
-                res = num1 * num2;
-                System.out.println("Результат:" + res);
-                break;
-            case "/":
-                if (num2 == 0)
-                    System.out.println("На 0 делить нельзя");
-                else {
-                    res = num1 / num2;
-                    System.out.println("Результат::" + res);
-
-                }
-                break;
-            default:
-
-                System.out.println("Что-ьо пошло не так");
-
-
-
+        Matcher matcher = pattern.matcher(input);
+        if (!matcher.matches()) {
+            throw new Exception("throws Exception");
         }
+        String[] parts = input.split("\\s+");
+        int num1 = Integer.parseInt(parts[0]);
+        String operator = parts[1];
+        int num2 = Integer.parseInt(parts[2]);
+        return switch (operator) {
+            case "+" -> String.valueOf(num1 + num2);
+            case "-" -> String.valueOf(num1 - num2);
+            case "*" -> String.valueOf(num1 * num2);
+            case "/" -> String.valueOf(num1 / num2);
+            default -> throw new Exception("throws Exception");
+        };
     }
 }
 
